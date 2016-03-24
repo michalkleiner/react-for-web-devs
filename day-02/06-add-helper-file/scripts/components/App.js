8bc52e305fd4be3e5fb60f4502e61312ce1da706
Jonathan Stark <jstark@jonathanstark.com>
2016-03-24 05:12:01 -0400

    added helper file for the date formatting

---
+++ day-02/06-add-helper-file/scripts/components/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Summary from './Summary';
+import Controls from './Controls';
+import EntryTable from './EntryTable';
+import u from '../helpers';
+
+var App = React.createClass({
+    getInitialState: function() {
+        return {
+            calories: 520,
+            date: '2016-01-05',
+            entries: {
+                entry1: {
+                    type: 'Food',
+                    description: 'Bagel',
+                    calories: 400,
+                    date: '2016-01-05',
+                    time: '12:34'
+                },
+                entry2: {
+                    type: 'Food',
+                    description: 'OJ',
+                    calories: 120,
+                    date: '2016-01-05',
+                    time: '12:34'
+                }
+            }
+        }
+    },
+    deleteEntry: function(key) {
+        delete this.state.entries[key];
+        this.setState({entries : this.state.entries});
+    },
+    setDate: function(offset) {
+        var date = new Date(this.state.date);
+        date.setUTCDate(date.getUTCDate() + offset);
+        this.state.date = u.dbDate(date);
+        this.setState({date : this.state.date});
+    },
+    render: function() {
+        return (
+            <section className="home">
+                <Summary date={this.state.date} calories={this.state.calories} />
+                <Controls setDate={this.setDate} />
+                <EntryTable
+                    date={this.state.date}
+                    entries={this.state.entries}
+                    deleteEntry={this.deleteEntry} />
+            </section>
+        )
+    }
+});
+
+export default App;
---
+++ day-02/06-add-helper-file/scripts/helpers.js
@@ -0,0 +1,18 @@
+let helpers = {
+    prettyDate: function(utcDateString) {
+        var weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+        var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        var d = new Date(utcDateString);
+        var prettyDate = weekdays[d.getUTCDay()] + ' ' + months[d.getUTCMonth()] + ' ' + d.getUTCDate();
+        return prettyDate;
+    },
+    dbDate: function(dateObject) {
+        var dd = ('0' + dateObject.getUTCDate()).slice(-2);
+        var mm = ('0' + (dateObject.getUTCMonth() + 1)).slice(-2);
+        var yyyy = dateObject.getUTCFullYear();
+        var dateString = yyyy + '-'+ mm + '-'+ dd;
+        return dateString;
+    }
+}
+
+export default helpers;
